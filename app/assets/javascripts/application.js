fb427cc90ab84df71cf1b41a092ba10a3cdd7e1f
serknight <chrisknight.mail@gmail.com>
2015-04-16 12:43:53 -0600

    asset_url and javascript pain in but

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,60 +15,15 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require typed
+//= require homepage
 //= require_tree .
 
 
 $(document).ready(function(){
-	homepageInit();
+	appInit();
 })
 
 
-function homepageInit() {
-	$('.looking-glass').css('height', $(window).height())
-
-	$(function(){
-	  $(window).load(function(){
-	    $(".element").typed({
-	      strings: ["web development", "ruby on rails", "javascript", "Anthropologist Programmer"],
-	      typeSpeed: 0,
-	      backspeed: 20000
-	    });
-	  })
-	});
-
-
-
-	$(function(){
-	  var current;
-	  $(document).mousemove(function(cursor){
-	      var left = cursor.pageX, top = cursor.pageY, win = $(window),
-	          width = win.width(), height = win.height(), horizontal, vertical, id;
-
-	      if( left < width / 3 ){
-	        horizontal = "left";
-	      } else if ( (left > width / 3) && ( left < (width - width / 3)) ){
-	        horizontal = "center";
-	      } else if (left > width / 3) {
-	        horizontal = "right";
-	      }
-	      if( top < height / 3 ){
-	        vertical = "top";
-	      } else if ( (top > height / 3) && ( top < (height - height / 3)) ){
-	        vertical = "center";
-	      } else if (top > height / 3) {
-	        vertical = "bot";
-	      }
-
-	      id = vertical + horizontal;
-	      
-	      if(id == current) {
-          return;
-	      }
-
-	      console.log("id => " + id)
-	     
-	      current = id;
-	      $(".topher-face img").attr('src', '/assets/'+ id + ".JPG");
-	  });
-	})	
+function appInit(){
+	homepageInit();	
 }
