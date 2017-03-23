17e788118fa65784c8792233fb07275fbebd1553
serknight <chrisknight.mail@gmail.com>
2017-03-23 16:20:33 -0400

    yolo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def homepage
-  	#@homepage_photo ||= Unsplash::Photo.search("space").take(1).first
-  	@rot_advs = ['home1.jpeg','home2.jpeg','home3.jpeg']
-  	@homepage_img_url = @rot_advs.sample
+
   end
 
 
